45f2e1b29b131b0a2f78126f096fcf2080ac02a5
hummingbird09 <167159194+hummingbird09@users.noreply.github.com>
2025-10-11 09:38:51 +0000

    dataTypes

---
+++ 01_basics/02_dataTypes.js
@@ -0,0 +1,23 @@
+"use strict"            //treat all the js code as newer version
+//alert(3+3)          // twe are using node js, not browser
+
+let name = "ashika"
+
+
+
+/*
+number => 2^53
+bigint
+string => ""
+boolean => true/false  
+ null => standalone value
+ undefined 
+ object 
+
+*/
+
+console.log(typeof name);       //string
+console.log(typeof undefined);      // undefined
+console.log(typeof null);           //object
+
+
