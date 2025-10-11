058439bad0d76075185aebba0bd5ccca042bd310
hummingbird09 <167159194+hummingbird09@users.noreply.github.com>
2025-10-11 10:19:59 +0000

    conversion of datatypes

---
+++ 01_basics/03_changeDt.js
@@ -0,0 +1,21 @@
+let num = "33";
+
+console.log(typeof (num));
+
+let changeToInt = Number(num);
+console.log(typeof (changeToInt));
+
+// "33" => 33
+// "33abc" => NaN
+// "meowmeow" => NaN
+// true => 1 and false => 0
+// null => 0;
+
+let isLoggedIn = 1;
+let booleanIsLoggedIn = Boolean(isLoggedIn);
+console.log(booleanIsLoggedIn);
+
+// 1 => true or 0 => false
+// "" => false
+// "Ashika" => true
+
